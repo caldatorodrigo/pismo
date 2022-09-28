b96e35186a1f6e9df8f671177b201261d3125057
rodrigocaldato <rodrigo.caldato@dasa.com.br>
2022-09-28 19:03:41 -0300

    fix: remove files

--- src/main/java/com/pismo/backtest/interfaces/OperationsTypeInterface.java
+++
@@ -1,9 +0,0 @@
-package com.pismo.backtest.interfaces;
-
-import org.springframework.http.ResponseEntity;
-
-import com.pismo.backtest.model.DTO.OperationsTypesDTO;
-
-public interface OperationsTypeInterface {
-	ResponseEntity<Object> save(OperationsTypesDTO operationsTypesDto);
-}
--- src/main/java/com/pismo/backtest/interfaces/TransactionServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.pismo.backtest.interfaces;
-
-import org.springframework.http.ResponseEntity;
-
-import com.pismo.backtest.model.DTO.TransactionsDTO;
-
-public interface TransactionServiceInterface {
-	ResponseEntity<Object> save(TransactionsDTO transactionDto);
-}
